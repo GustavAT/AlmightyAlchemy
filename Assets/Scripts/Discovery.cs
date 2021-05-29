10a317dd2662b56c2e82d8f4ae18ed0cf5b05177
Andreas Tscheinig <andreas-tscheinig@gmx.at>
2021-05-29 17:52:58 +0200

    Fix discovery animation

--- Assets/Scripts/Discovery.cs
+++ Assets/Scripts/Discovery.cs
@@ -25,7 +25,8 @@ public class Discovery : MonoBehaviour
     /// </summary>
     private readonly Queue<Item> _itemQueue = new Queue<Item>();
 
-    private static readonly int DiscoveryClose = Animator.StringToHash("DiscoveryClose");
+    private static readonly int HideDiscovery = Animator.StringToHash("HideDiscovery");
+    private static readonly int ShowDiscovery = Animator.StringToHash("ShowDiscovery");
 
     public void AddItem(Item item)
     {
@@ -45,20 +46,21 @@ public class Discovery : MonoBehaviour
 
     private void ClosePanel()
     {
-        DiscoveryAnimator.SetTrigger(DiscoveryClose);
-        Invoke(nameof(DeActivePanel), 0.3f);
+        DiscoveryAnimator.SetTrigger(HideDiscovery);
+        Invoke(nameof(DeActivePanel), 0.5f);
         
     }
 
     public void DeActivePanel()
     {
+        Debug.Log("Deactivate Panel");
         DiscoveryPanel.SetActive(false);
     }
     
     private void OpenPanel()
     {
         DiscoveryPanel.SetActive(true);
-        DiscoveryAnimator.Play("DiscoveryOpen");
+        DiscoveryAnimator.SetTrigger(ShowDiscovery);
 
         var item = _itemQueue.Dequeue();
         var presenter = DiscoveryPanel.GetComponent<DiscoveryPresenter>();
